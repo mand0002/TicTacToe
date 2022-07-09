b3396ad30aab7008bae52cd543a9cc1e8555d272
Heather Kossila <57233152+mand0002@users.noreply.github.com>
2022-07-08 22:21:10 -0500

    made more changes

--- colorGame00.jsx
+++ colorGame00.jsx
@@ -1,47 +1,75 @@
-const Square = ({ id, player })=>{
-    const [color,setColor] = React.useState('green');
+const Square = ({ id, player, newState })=>{
+    const [color, setColor] = React.useState('green');
+    const [status, setStatus] = React.useState(null);
+    const XorO= ["O","X"];
+  
     const palet = ['red', 'blue', 'green'];
     const getRandomColor =()=> palet[Math.floor(Math.random()*3)];
+   
     React.useEffect(()=>{
         console.log(`Render ${id}`);
         return ()=> console.log(`unmounting Square ${id}`);
-    })
-
-
+    });
     //keep track of state of square
     return (
         //change color of square on click
     <button 
         onClick={e =>  {
-        setColor(getRandomColor());
-        e.target.style.background = color;
+        let col = getRandomColor();
+        setColor(col);
+        let nextplayer = newState(id);
+        setStatus(nextplayer);
+        e.target.style.background = col;
     }}>
-        <h1>{player}</h1>
+        <h1>{XorO[status]}</h1>
     </button>
     );
 };
 
 const Board = () => {
-  const [player, setPlayer] = React.useState(0);
-  const [mounted, setMounted] = React.useState(true);
-  const [random, setRandom] = React.useState(0);
+  const [player, setPlayer] = React.useState(1);
+  const [state, setState] = React.useState(Array(9).fill(null));
   let status = `Player ${player}`;
-  const toggle = ()=> setMounted(!mounted);
-  const reRender = ()=>setRandom(Math.random());
+  let winner = checkForWinner(state);
+  if(winner != null) status = `Player ${winner} wins`
+
+  const newState = idOfSquare => {
+    let thePlayer = player;
+    state[idOfSquare] = player; //player is present player
+    setState(state); //state is array of 0 or 1 or null
+    let nextplayer = (player + 1) %2;
+    setPlayer(nextplayer);
+
+    return thePlayer;  //we need to return the present player
+  }
+
+  //const toggle = ()=> setMounted(!mounted);
+  //const reRender = ()=>setRandom(Math.random());
+  
   function renderSquare(i) {
-    return <Square id={i} player={player}></Square>;
+    return <Square id={i} newState={newState}></Square>;
   }
   return (
     <div className="game-board">
       <div className="grid-row">
-        {mounted && renderSquare(0)}
-        {mounted && renderSquare(1)}
-        {mounted && renderSquare(2)}
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="grid-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="grid-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
       <div id="info">
-        <button onClick={toggle}>Show/Hide Row</button>
-        <button onClick={reRender}>Re-render</button>
-        <h1>{status}</h1>
+        <button>Show/Hide Row</button>
+        <button>Re-render</button>
+        <h1> {status} </h1>
       </div>
     </div>
   );
